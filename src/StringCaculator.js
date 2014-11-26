119b70b10a31fdcb44d3d2d6d593da5837c8cccb
zhangekehbg <itzhangke@gmail.com>
2014-11-26 15:16:59 +0800

    add new file 1.txt

--- src/StringCaculator.js
+++ src/StringCaculator.js
@@ -8,17 +8,17 @@ StringCaculator.prototype.add = function () {
     var sum = 0;
     var string = _.toArray(arguments[0]);
     for (var i = 0; i < string.length; i++) {
-        if((string[i]) ==='-'){
+        if ((string[i]) === '-') {
             return 'negatives not allowed';
         }
         if (parseInt(string[i])) {
-            while (parseInt(string[i + 1])||parseInt(string[i + 1]) ==0) {
+            while (parseInt(string[i + 1]) || parseInt(string[i + 1]) == 0) {
                 string[i + 1] = string[i] + string[i + 1];
                 i++;
             }
             ;
         }
-        if (parseInt(string[i])&& parseInt(string[i])<1000) {
+        if (parseInt(string[i]) && parseInt(string[i]) < 1000) {
             sum += parseInt(string[i]);
         }
 
